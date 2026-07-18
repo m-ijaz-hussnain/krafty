5498ccdf01956179aae7b6c7037f58652ab7e21c
M IJAZ HUSSNAIN <mijazhussnain83@gmail.com>
2026-07-18 22:44:33 +0500

    Added project details and images
    Uploaded project documentation files along with relevant image assets.

--- mocks/projects.js
+++ mocks/projects.js
@@ -1,349 +1,521 @@
 const projects = [
   {
-    title: "EcoTrack",
-    description: "Carbon footprint reduction app.",
+    title: "Harringtons",
+    description: "Luxury residential real estate project.",
     introduction:
-      "EcoTrack is a revolutionary mobile application designed to help individuals and organizations track and reduce their carbon footprint. In an era where environmental consciousness is more crucial than ever, EcoTrack serves as a vital tool for sustainable living.",
+      "",
     images: [
-      "/images/projects/eco-track.png",
-      "/images/projects/medi-connect.png",
-      "/images/projects/aero-scan.png",
-      "/images/projects/learn-sphere.png",
+      "/images/projects/harringtons1.jpeg",
+      "/images/projects/harringtons2.jpeg",
+      "/images/projects/harringtons3.jpeg",
+      "/images/projects/harringtons4.jpeg",
     ],
     features: [
       {
-        title: "Tips",
+        title: "",
       },
       {
-        title: "Analytics",
+        title: "",
       },
       {
-        title: "Challenges",
+        title: "",
       },
     ],
     problem: [
       {
-        title: "Rising Carbon Footprint:",
+        title: "",
         description:
-          "The increasing carbon emissions from transportation, energy usage, and daily activities contribute significantly to global warming and climate change.",
+          "",
       },
       {
-        title: "Lack of Awareness:",
+        title: "",
         description:
-          "Many people and organizations are unaware of the extent of their carbon footprint and how their daily choices impact the environment.",
+          "",
       },
       {
-        title: "Difficulty in Tracking:",
+        title: "",
         description:
-          "Without a simple and effective way to track carbon emissions, it's challenging for individuals and businesses to implement practical changes towards sustainability.",
+          "",
       },
     ],
     solution: [
       {
-        title: "Carbon Footprint Calculator:",
+        title: "",
         description:
-          "The app calculates your carbon footprint based on your daily activities, like commuting, energy consumption, and travel.",
+          "",
       },
       {
-        title: "Personalized Tips:",
+        title: "",
         description:
-          "Receive tailored advice on reducing your carbon footprint in various aspects of your life.",
+          "",
       },
       {
-        title: "Progress Tracking:",
+        title: "",
         description:
-          " Monitor your carbon reduction journey over time and see the impact of your efforts.",
+          "",
       },
     ],
-
     benefits: [
       {
-        title: "Increased Awareness",
+        title: "",
       },
       {
-        title: "Practical Steps",
+        title: "",
       },
       {
-        title: "Community Engagement",
+        title: "",
       },
       {
-        title: "Impactful Change",
+        title: "",
       },
     ],
 
     introductions: [
       {
-        title: "Download the App",
+        title: "",
       },
       {
-        title: "Set Your Profile",
+        title: "",
       },
       {
-        title: "Start Tracking",
+        title: "",
       },
       {
-        title: "Engage with Community",
+        title: "",
       },
     ],
   },
   {
-    title: "MediConnect",
-    description: "Virtual healthcare consultation platform.",
+    title: "Ohana",
+    description: "Boutique luxury villas or residences.",
     introduction:
-      "EcoTrack is a revolutionary mobile application designed to help individuals and organizations track and reduce their carbon footprint. In an era where environmental consciousness is more crucial than ever, EcoTrack serves as a vital tool for sustainable living.",
+      "",
     images: [
-      "/images/projects/medi-connect.png",
-      "/images/projects/eco-track.png",
-      "/images/projects/aero-scan.png",
-      "/images/projects/learn-sphere.png",
+      "/images/projects/ohana1.jpeg",
+      "/images/projects/ohana2.jpeg",
+      "/images/projects/ohana3.jpeg",
+      "/images/projects/ohana4.jpeg",
     ],
     features: [
       {
-        title: "Tips",
+        title: "",
       },
       {
-        title: "Analytics",
+        title: "",
       },
       {
-        title: "Challenges",
+        title: "",
       },
     ],
     problem: [
       {
-        title: "Rising Carbon Footprint:",
+        title: "",
         description:
-          "The increasing carbon emissions from transportation, energy usage, and daily activities contribute significantly to global warming and climate change.",
+          "",
       },
       {
-        title: "Lack of Awareness:",
+        title: "",
         description:
-          "Many people and organizations are unaware of the extent of their carbon footprint and how their daily choices impact the environment.",
+          "",
       },
       {
-        title: "Difficulty in Tracking:",
+        title: "",
         description:
-          "Without a simple and effective way to track carbon emissions, it's challenging for individuals and businesses to implement practical changes towards sustainability.",
+          "",
       },
     ],
     solution: [
       {
-        title: "Carbon Footprint Calculator:",
+        title: "",
         description:
-          "The app calculates your carbon footprint based on your daily activities, like commuting, energy consumption, and travel.",
+          "",
       },
       {
-        title: "Personalized Tips:",
+        title: "",
         description:
-          "Receive tailored advice on reducing your carbon footprint in various aspects of your life.",
+          "",
       },
       {
-        title: "Progress Tracking:",
+        title: "",
         description:
-          " Monitor your carbon reduction journey over time and see the impact of your efforts.",
+          "",
       },
     ],
     benefits: [
       {
-        title: "Increased Awareness",
+        title: "",
       },
       {
-        title: "Practical Steps",
+        title: "",
       },
       {
-        title: "Community Engagement",
+        title: "",
       },
       {
-        title: "Impactful Change",
+        title: "",
       },
     ],
 
     introductions: [
       {
-        title: "Download the App",
+        title: "",
       },
       {
-        title: "Set Your Profile",
+        title: "",
       },
       {
-        title: "Start Tracking",
+        title: "",
       },
       {
-        title: "Engage with Community",
+        title: "",
+      },
+    ], 
+  },
+  {
+    title: "Drill",
+    description: "Power tool for drilling holes.",
+    introduction:
+      "",
+    images: [
+      "/images/projects/drill1.jpeg",
+      "/images/projects/drill2.jpeg",
+      "/images/projects/drill3.jpeg",
+      "/images/projects/drill4.jpeg",
+    ],
+    features: [
+      {
+        title: "",
+      },
+      {
+        title: "",
+      },
+      {
+        title: "",
+      },
+    ],
+    problem: [
+      {
+        title: "",
+        description:
+          "",
+      },
+      {
+        title: "",
+        description:
+          "",
+      },
+      {
+        title: "",
+        description:
+          "",
+      },
+    ],
+    solution: [
+      {
+        title: "",
+        description:
+          "",
+      },
+      {
+        title: "",
+        description:
+          "",
+      },
+      {
+        title: "",
+        description:
+          "",
+      },
+    ],
+    benefits: [
+      {
+        title: "",
+      },
+      {
+        title: "",
+      },
+      {
+        title: "",
+      },
+      {
+        title: "",
+      },
+    ],
+
+    introductions: [
+      {
+        title: "",
+      },
+      {
+        title: "",
+      },
+      {
+        title: "",
+      },
+      {
+        title: "",
       },
     ],
   },
   {
-    title: "AeroScan",
-    description: "Drone-powered infrastructure inspection.",
+    title: "Financela",
+    description: "Financial technology or services platform.",
     introduction:
-      "EcoTrack is a revolutionary mobile application designed to help individuals and organizations track and reduce their carbon footprint. In an era where environmental consciousness is more crucial than ever, EcoTrack serves as a vital tool for sustainable living.",
+      "",
     images: [
-      "/images/projects/aero-scan.png",
-      "/images/projects/medi-connect.png",
-      "/images/projects/eco-track.png",
-      "/images/projects/learn-sphere.png",
+      "/images/projects/financela1.jpeg",
+      "/images/projects/financela2.jpeg",
+      "/images/projects/financela3.jpeg",
+      "/images/projects/financela4.jpeg",
     ],
     features: [
       {
-        title: "Tips",
+        title: "",
       },
       {
-        title: "Analytics",
+        title: "",
       },
       {
-        title: "Challenges",
+        title: "",
       },
     ],
     problem: [
       {
-        title: "Rising Carbon Footprint:",
+        title: "",
         description:
-          "The increasing carbon emissions from transportation, energy usage, and daily activities contribute significantly to global warming and climate change.",
+          "",
       },
       {
-        title: "Lack of Awareness:",
+        title: "",
         description:
-          "Many people and organizations are unaware of the extent of their carbon footprint and how their daily choices impact the environment.",
+          "",
       },
       {
-        title: "Difficulty in Tracking:",
+        title: "",
         description:
-          "Without a simple and effective way to track carbon emissions, it's challenging for individuals and businesses to implement practical changes towards sustainability.",
+          "",
       },
     ],
     solution: [
       {
-        title: "Carbon Footprint Calculator:",
+        title: "",
         description:
-          "The app calculates your carbon footprint based on your daily activities, like commuting, energy consumption, and travel.",
+          "",
       },
       {
-        title: "Personalized Tips:",
+        title: "",
         description:
-          "Receive tailored advice on reducing your carbon footprint in various aspects of your life.",
+          "",
       },
       {
-        title: "Progress Tracking:",
+        title: "",
         description:
-          " Monitor your carbon reduction journey over time and see the impact of your efforts.",
+          "",
       },
     ],
     benefits: [
       {
-        title: "Increased Awareness",
+        title: "",
       },
       {
-        title: "Practical Steps",
+        title: "",
       },
       {
-        title: "Community Engagement",
+        title: "",
       },
       {
-        title: "Impactful Change",
+        title: "",
       },
     ],
 
     introductions: [
       {
-        title: "Download the App",
+        title: "",
       },
       {
-        title: "Set Your Profile",
+        title: "",
       },
       {
-        title: "Start Tracking",
+        title: "",
       },
       {
-        title: "Engage with Community",
+        title: "",
       },
     ],
   },
   {
-    title: "LearnSphere",
-    description: "Immersive online education platform.",
+    title: "CarismaCar",
+    description: "Mitsubishi hatchback or sedan model.",
     introduction:
-      "EcoTrack is a revolutionary mobile application designed to help individuals and organizations track and reduce their carbon footprint. In an era where environmental consciousness is more crucial than ever, EcoTrack serves as a vital tool for sustainable living.",
+      "",
     images: [
-      "/images/projects/learn-sphere.png",
-      "/images/projects/aero-scan.png",
-      "/images/projects/medi-connect.png",
-      "/images/projects/eco-track.png",
+      "/images/projects/carisma1.jpeg",
+      "/images/projects/carisma2.jpeg",
+      "/images/projects/carisma3.jpeg",
+      "/images/projects/carisma5.jpeg",
     ],
     features: [
       {
-        title: "Tips",
+        title: "",
       },
       {
-        title: "Analytics",
+        title: "",
       },
       {
-        title: "Challenges",
+        title: "",
       },
     ],
     problem: [
       {
-        title: "Rising Carbon Footprint:",
+        title: "",
         description:
-          "The increasing carbon emissions from transportation, energy usage, and daily activities contribute significantly to global warming and climate change.",
+          "",
       },
       {
-        title: "Lack of Awareness:",
+        title: "",
         description:
-          "Many people and organizations are unaware of the extent of their carbon footprint and how their daily choices impact the environment.",
+          "",
       },
       {
-        title: "Difficulty in Tracking:",
+        title: "",
         description:
-          "Without a simple and effective way to track carbon emissions, it's challenging for individuals and businesses to implement practical changes towards sustainability.",
+          "",
       },
     ],
     solution: [
       {
-        title: "Carbon Footprint Calculator:",
+        title: "",
         description:
-          "The app calculates your carbon footprint based on your daily activities, like commuting, energy consumption, and travel.",
+          "",
       },
       {
-        title: "Personalized Tips:",
+        title: "",
         description:
-          "Receive tailored advice on reducing your carbon footprint in various aspects of your life.",
+          "",
       },
       {
-        title: "Progress Tracking:",
+        title: "",
         description:
-          " Monitor your carbon reduction journey over time and see the impact of your efforts.",
+          "",
       },
     ],
     benefits: [
       {
-        title: "Increased Awareness",
+        title: "",
       },
       {
-        title: "Practical Steps",
+        title: "",
       },
       {
-        title: "Community Engagement",
+        title: "",
       },
       {
-        title: "Impactful Change",
+        title: "",
       },
     ],
 
     introductions: [
       {
-        title: "Download the App",
+        title: "",
       },
       {
-        title: "Set Your Profile",
+        title: "",
       },
       {
-        title: "Start Tracking",
+        title: "",
       },
       {
-        title: "Engage with Community",
+        title: "",
       },
     ],
   },
+  
+  // {
+  //   title: "LearnSphere",
+  //   description: "Immersive online education platform.",
+  //   introduction:
+  //     "EcoTrack is a revolutionary mobile application designed to help individuals and organizations track and reduce their carbon footprint. In an era where environmental consciousness is more crucial than ever, EcoTrack serves as a vital tool for sustainable living.",
+  //   images: [
+  //     "/images/projects/learn-sphere.png",
+  //     "/images/projects/aero-scan.png",
+  //     "/images/projects/medi-connect.png",
+  //     "/images/projects/eco-track.png",
+  //   ],
+  //   features: [
+  //     {
+  //       title: "Tips",
+  //     },
+  //     {
+  //       title: "Analytics",
+  //     },
+  //     {
+  //       title: "Challenges",
+  //     },
+  //   ],
+  //   problem: [
+  //     {
+  //       title: "Rising Carbon Footprint:",
+  //       description:
+  //         "The increasing carbon emissions from transportation, energy usage, and daily activities contribute significantly to global warming and climate change.",
+  //     },
+  //     {
+  //       title: "Lack of Awareness:",
+  //       description:
+  //         "Many people and organizations are unaware of the extent of their carbon footprint and how their daily choices impact the environment.",
+  //     },
+  //     {
+  //       title: "Difficulty in Tracking:",
+  //       description:
+  //         "Without a simple and effective way to track carbon emissions, it's challenging for individuals and businesses to implement practical changes towards sustainability.",
+  //     },
+  //   ],
+  //   solution: [
+  //     {
+  //       title: "Carbon Footprint Calculator:",
+  //       description:
+  //         "The app calculates your carbon footprint based on your daily activities, like commuting, energy consumption, and travel.",
+  //     },
+  //     {
+  //       title: "Personalized Tips:",
+  //       description:
+  //         "Receive tailored advice on reducing your carbon footprint in various aspects of your life.",
+  //     },
+  //     {
+  //       title: "Progress Tracking:",
+  //       description:
+  //         " Monitor your carbon reduction journey over time and see the impact of your efforts.",
+  //     },
+  //   ],
+  //   benefits: [
+  //     {
+  //       title: "Increased Awareness",
+  //     },
+  //     {
+  //       title: "Practical Steps",
+  //     },
+  //     {
+  //       title: "Community Engagement",
+  //     },
+  //     {
+  //       title: "Impactful Change",
+  //     },
+  //   ],
+
+  //   introductions: [
+  //     {
+  //       title: "Download the App",
+  //     },
+  //     {
+  //       title: "Set Your Profile",
+  //     },
+  //     {
+  //       title: "Start Tracking",
+  //     },
+  //     {
+  //       title: "Engage with Community",
+  //     },
+  //   ],
+  // },
 ];
 
 export { projects };
