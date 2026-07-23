640e125f2556c41657e29131bdbf20b1a944739a
M IJAZ HUSSNAIN <mijazhussnain83@gmail.com>
2026-07-23 16:24:46 +0500

    Update service image assets

--- mocks/services.js
+++ mocks/services.js
@@ -2,17 +2,17 @@ const services = [
   {
     title: "UI/UX Design",
     cost: "Starts at $999",
-    image: "/images/projects/financela1.jpeg",
+    image: "/images/projects/ohana5.jpeg",
   },
   {
     title: "Website & Landing Page Design",
     cost: "Starts at $1,499",
-    image: "/images/projects/ohana5.jpeg",
+    image: "/images/projects/financela1.jpeg",
   },
   {
     title: "Web & Mobile App Design",
     cost: "Starts at $1,999",
-    image: "/images/projects/drill3.jpeg",
+    image: "/images/projects/drill5.jpeg",
   },
   {
     title: "SaaS Product Design",
